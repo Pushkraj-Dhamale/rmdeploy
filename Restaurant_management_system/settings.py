d49a0a8e0b79bc664f578ba9d886f970714073a4
Pushkraj Dhamale <102896317+Pushkraj-Dhamale@users.noreply.github.com>
2023-04-21 22:54:41 +0530

    Update settings.py

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
 
 ]
 
@@ -135,9 +134,6 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-import os
-STATICFILES_DIRS= os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # SMTP Configuration
 
