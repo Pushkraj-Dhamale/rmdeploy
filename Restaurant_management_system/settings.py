a6a925b3c6b3008b127a9fafef33c6d09bd1c240
Pushkraj Dhamale <102896317+Pushkraj-Dhamale@users.noreply.github.com>
2023-04-23 22:09:58 +0530

    Update settings.py

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.urls import re_path as url
+from django.views.static import serve
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT='staticfiles'
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
